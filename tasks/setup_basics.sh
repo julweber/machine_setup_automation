0cb3c2a8c19a9eaad67043743f534a93f1ad3a44
Julian Weber <julianweberdev@gmail.com>
2025-11-12 11:29:33 +0100

    cleanup setup basics task

--- tasks/setup_basics.sh
+++ tasks/setup_basics.sh
@@ -2,7 +2,26 @@
 set -eu
 
 sudo apt update
-sudo apt install -y curl tldr bat git python3-full python3-pip jq yq net-tools wget gpg  netcat-openbsd terminator libfuse2 gnome-control-center gnome-online-accounts openssh-server nvtop btop htop
+sudo apt install -y curl \
+    tldr \
+    bat \
+    git \
+    python3-full \
+    python3-pip \
+    jq \
+    yq \
+    net-tools \
+    wget \
+    gpg \
+    netcat-openbsd \
+    terminator \
+    libfuse2 \
+    gnome-control-center \
+    gnome-online-accounts \
+    nvtop \
+    radeontop \
+    btop \
+    htop
 
 # Python basics
 pip install uv
