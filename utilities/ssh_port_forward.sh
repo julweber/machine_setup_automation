7c6f6c68f37b9ef3016c4c1498a53fe806ab1581
Julian Weber <julianweberdev@gmail.com>
2025-11-12 12:56:34 +0100

    utilities - ssh port forward - add usage example

--- utilities/ssh_port_forward.sh
+++ utilities/ssh_port_forward.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# usage
+# general: 
+# ./ssh_forward_port.sh $LOCAL_PORT $TARGET_MACHINE $REMOTE_PORT $TARGET_MACHINE_SSH_USER $TARGET_MACHINE_SSH_PORT
+# e.g.:
+# ./ssh_forward_port.sh 3333 192.168.0.3 3333 myuser 2224
+
 # Parameters
 LOCAL_PORT=$1
 REMOTE_HOST=$2
