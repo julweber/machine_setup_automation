5aba2cb0d451464e2dd083b3a08e24668de15e3f
Julian Weber <julianweberdev@gmail.com>
2026-03-05 10:07:48 +0100

    adds setup for anydesk

---
+++ tasks/setup_anydesk.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+sudo apt update
+
+sudo apt install ca-certificates curl apt-transport-https
+
+sudo install -m 0755 -d /etc/apt/keyrings
+
+sudo curl -fsSL https://keys.anydesk.com/repos/DEB-GPG-KEY -o /etc/apt/keyrings/keys.anydesk.com.asc
+
+sudo chmod a+r /etc/apt/keyrings/keys.anydesk.com.asc
+
+echo "deb [signed-by=/etc/apt/keyrings/keys.anydesk.com.asc] https://deb.anydesk.com all main" | sudo tee /etc/apt/sources.list.d/anydesk-stable.list
+
+sudo apt update
+
+sudo apt install anydesk
+ 
