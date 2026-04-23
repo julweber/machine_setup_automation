7bccfb02b264dd277fcb29e060307295669a82a2
Julian Weber <julianweberdev@gmail.com>
2026-04-23 10:26:12 +0200

    remove lm studio setup call from server setup

--- setup-dev-machine.sh
+++ setup-dev-machine.sh
@@ -3,19 +3,8 @@ set -eu
 
 source tasks/setup-basics.sh
 source tasks/setup-sshd.sh
+source tasks/configure-firewall.sh
 source tasks/setup-docker.sh
 source tasks/setup-lm-studio.sh
-source tasks/configure-firewall.sh
 
-# source tasks/setup-nanobot.sh
-
-# source tasks/setup-kubernetes.sh
-# source tasks/setup-openwebui.sh
-
-# source tasks/setup-rocm.sh
-
-# source tasks/setup-brave.sh
-# source tasks/setup-vscode.sh
-# source tasks/setup-opencode-server.sh
-# source tasks/setup-pi.sh
-# source tasks/setup-hyprwhspr.sh
+# for further configurations use the setup scripts in tasks/
--- setup-server.sh
+++ setup-server.sh
@@ -3,17 +3,7 @@ set -eu
 
 source tasks/setup-basics.sh
 source tasks/setup-sshd.sh
-source tasks/setup-docker.sh
-source tasks/setup-lm-studio.sh
 source tasks/configure-firewall.sh
+source tasks/setup-docker.sh
 
-# source tasks/setup-nanobot.sh
-
-# source tasks/setup-rocm.sh
-
-# source tasks/setup-kubernetes.sh
-# source tasks/setup-samba.sh
-# source tasks/setup-opencode-server.sh
-# source tasks/setup-pi.sh
-
-
+# for further configurations use the setup scripts in tasks/
